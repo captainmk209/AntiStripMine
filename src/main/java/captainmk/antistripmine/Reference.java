7e28d6f4ba66627fc9e09b8ce61b39b96685704e
captainmk <captainmk@cs.nmt.edu>
2019-10-11 20:50:46 -0400

    Fixed gradle output, updated icon size, incremented version number

--- src/main/java/captainmk/antistripmine/Reference.java
+++ src/main/java/captainmk/antistripmine/Reference.java
@@ -1,8 +1,8 @@
 package captainmk.antistripmine;
 
 public class Reference {
-    public static final String MODID = "cmk_asm";
+    public static final String MODID = "cmk_antistripmine";
     public static final String MODNAME = "Anti-StripMine";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
     public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
 }
